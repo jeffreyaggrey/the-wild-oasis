c40c470edd3e57b1d5d7959aa38075f6cff4ebd7
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-03-05 19:01:13 -0600

    setup styled-components, create ui Button and Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const StyledApp = styled.div`
+  background-color: burlywood;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Check in')}>Click Me</Button>
+        <Input type="text" placeholder="Enter your name" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
