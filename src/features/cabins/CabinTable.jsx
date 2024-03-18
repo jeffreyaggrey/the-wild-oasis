50dabf37946204e364094412a49e524e8881146a
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-03-17 20:51:11 -0500

    create a Context Menu for actions in CabinRow

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import CabinRow from './CabinRow';
 import Table from '../../ui/Table';
 
 import { useCabins } from './useCabins';
+import Menus from '../../ui/Menus';
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -10,21 +11,23 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
 
-      <Table.Body
-        data={cabins}
-        render={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
-      />
-    </Table>
+        <Table.Body
+          data={cabins}
+          render={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 
