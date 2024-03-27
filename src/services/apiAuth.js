95f598dd286ff12e3833f95e4759a1215d24288b
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-03-27 05:39:07 -0500

    implement logout

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,16 +1,5 @@
 import supabase from './supabase';
 
-export async function login({ email, password }) {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) throw new Error(error.message);
-
-  return data;
-}
-
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
 
@@ -23,4 +12,21 @@ export async function getCurrentUser() {
   if (error) throw new Error(error.message);
 
   return data?.user;
+}
+
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
 }
